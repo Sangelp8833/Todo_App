571a55bcd5976f982e5e73df5a54d2127621f3a8
Samuel Angel Perez <sangelp@unal.edu.co>
2022-09-17 14:54:56 -0500

    Modificando views

--- lib/todo_app_web/views/task_view.ex
+++ lib/todo_app_web/views/task_view.ex
@@ -1,3 +1,10 @@
-defmodule TodoAppWeb.TaskView do
+efmodule TodoAppWeb.TaskView do
   use TodoAppWeb, :view
+
+  defp now() do
+    NaiveDateTime.utc_now() |> NaiveDateTime.to_string()
+  end
+
+  defp is_done(true), do: "It's done"
+  defp is_done(false), do: "Missing task"
 end
