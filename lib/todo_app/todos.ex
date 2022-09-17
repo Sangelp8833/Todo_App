ea48ca8ff10d178f5dda76595334cfb174148c21
Samuel Angel Perez <sangelp@unal.edu.co>
2022-09-17 15:04:38 -0500

    Agregando el contexto para todos

--- lib/todo_app/todos.ex
+++ lib/todo_app/todos.ex
@@ -10,12 +10,9 @@ defmodule TodoApp.Todos do
 
   @doc """
   Returns the list of tasks.
-
   ## Examples
-
       iex> list_tasks()
       [%Task{}, ...]
-
   """
   def list_tasks do
     Repo.all(Task)
@@ -23,31 +20,22 @@ defmodule TodoApp.Todos do
 
   @doc """
   Gets a single task.
-
   Raises `Ecto.NoResultsError` if the Task does not exist.
-
   ## Examples
-
       iex> get_task!(123)
       %Task{}
-
       iex> get_task!(456)
       ** (Ecto.NoResultsError)
-
   """
   def get_task!(id), do: Repo.get!(Task, id)
 
   @doc """
   Creates a task.
-
   ## Examples
-
       iex> create_task(%{field: value})
       {:ok, %Task{}}
-
       iex> create_task(%{field: bad_value})
       {:error, %Ecto.Changeset{}}
-
   """
   def create_task(attrs \\ %{}) do
     %Task{}
@@ -57,15 +45,11 @@ defmodule TodoApp.Todos do
 
   @doc """
   Updates a task.
-
   ## Examples
-
       iex> update_task(task, %{field: new_value})
       {:ok, %Task{}}
-
       iex> update_task(task, %{field: bad_value})
       {:error, %Ecto.Changeset{}}
-
   """
   def update_task(%Task{} = task, attrs) do
     task
@@ -75,15 +59,11 @@ defmodule TodoApp.Todos do
 
   @doc """
   Deletes a task.
-
   ## Examples
-
       iex> delete_task(task)
       {:ok, %Task{}}
-
       iex> delete_task(task)
       {:error, %Ecto.Changeset{}}
-
   """
   def delete_task(%Task{} = task) do
     Repo.delete(task)
@@ -91,12 +71,9 @@ defmodule TodoApp.Todos do
 
   @doc """
   Returns an `%Ecto.Changeset{}` for tracking task changes.
-
   ## Examples
-
       iex> change_task(task)
       %Ecto.Changeset{data: %Task{}}
-
   """
   def change_task(%Task{} = task, attrs \\ %{}) do
     Task.changeset(task, attrs)
